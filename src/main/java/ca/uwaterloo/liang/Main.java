6c5dcb57ec0c0e9a4833d3c5e1d5605234ef6d1c
dliang2000 <q8liang@uwaterloo.ca>
2020-05-16 02:56:06 -0400

    indentation fix

--- src/main/java/ca/uwaterloo/liang/Main.java
+++ src/main/java/ca/uwaterloo/liang/Main.java
@@ -60,20 +60,21 @@ public class Main {
             return instance;
         }
 
-	private boolean isDefinedOutsideTestClass(SootMethod m) {
-	    String ss = m.getSubSignature();
-	    SootClass sc = m.getDeclaringClass();
-	    // unfortunately, test classes are not annotated
-	    // we approximate by filtering out anything defined in java.*
-	    do {
-		if (!sc.declaresMethod(ss)) break;
-		if (sc.getJavaPackageName().startsWith("java")) {
-		    return true;
-		}
-		sc = sc.getSuperclass();
-	    } while (sc != null);
-	    return false;
-	}
+        private boolean isDefinedOutsideTestClass(SootMethod m) {
+            String ss = m.getSubSignature();
+            SootClass sc = m.getDeclaringClass();
+            // unfortunately, test classes are not annotated
+            // we approximate by filtering out anything defined in java.*
+            do {
+                if (!sc.declaresMethod(ss))
+                    break;
+                if (sc.getJavaPackageName().startsWith("java")) {
+                    return true;
+                }
+                sc = sc.getSuperclass();
+            } while (sc != null);
+            return false;
+        }
 
         @Override
         protected void internalTransform(String phaseName, Map options) {
